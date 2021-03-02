94150dfdc9405f702c1c9e16392bb9ff9e5dc7be
fallOut015 <captainDoodles264@gmail.com>
2021-03-02 01:24:44 -0800

    Update MainHUDQuiver.java

--- src/main/java/com/example/examplemod/MainHUDQuiver.java
+++ src/main/java/com/example/examplemod/MainHUDQuiver.java
@@ -9,24 +9,18 @@ import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ShootableItem;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -35,30 +29,10 @@ import java.util.function.Predicate;
 @Mod(MainHUDQuiver.MODID)
 public class MainHUDQuiver {
     public static final String MODID = "hud_quiver";
-    private static final Logger LOGGER = LogManager.getLogger();
-    //private static @Nullable ServerPlayerEntity player;
-    private static @Nullable ClientPlayerEntity player;
+    private static final ResourceLocation WIDGETS = new ResourceLocation("hud_quiver","textures/gui/widgets.png");
 
     public MainHUDQuiver() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-    private void doClientStuff(final FMLClientSetupEvent event) {
-    }
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-    }
-    private void processIMC(final InterModProcessEvent event) {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
     }
 
     public static List<ItemStack> findAmmos(PlayerEntity player, ItemStack shootable) {
@@ -71,19 +45,18 @@ public class MainHUDQuiver {
             ItemStack itemstack = ShootableItem.getHeldAmmo(player, predicate);
             if (!itemstack.isEmpty()) {
                 list.add(itemstack);
-            } else {
-                predicate = ((ShootableItem)shootable.getItem()).getInventoryAmmoPredicate();
+            }
+            predicate = ((ShootableItem)shootable.getItem()).getInventoryAmmoPredicate();
 
-                for(int i = 0; i < player.inventory.getSizeInventory(); ++i) {
-                    ItemStack itemstack1 = player.inventory.getStackInSlot(i);
-                    if (predicate.test(itemstack1)) {
-                        list.add(itemstack1);
-                    }
+            for(int i = 0; i < player.inventory.getSizeInventory(); ++i) {
+                ItemStack itemstack1 = player.inventory.getStackInSlot(i);
+                if (predicate.test(itemstack1) && itemstack1 != itemstack) {
+                    list.add(itemstack1);
                 }
+            }
 
-                if(list.isEmpty() && player.abilities.isCreativeMode) {
-                    list.add(new ItemStack(Items.ARROW));
-                }
+            if(list.isEmpty() && player.abilities.isCreativeMode) {
+                list.add(new ItemStack(Items.ARROW));
             }
 
             return list;
@@ -92,19 +65,13 @@ public class MainHUDQuiver {
 
     @Mod.EventBusSubscriber
     public static class Events {
-        /*@SubscribeEvent
-        public static void onPlayerTick(final TickEvent.PlayerTickEvent event) {
-            if(event.player instanceof ServerPlayerEntity) {
-                player = (ServerPlayerEntity) event.player;
-            }
-        }*/
         @OnlyIn(Dist.CLIENT)
         @SubscribeEvent
+        @SuppressWarnings("unused")
         public static void onRenderGameOverlay(final RenderGameOverlayEvent event) {
-            if(player == null) {
-                player = Minecraft.getInstance().player;
-            }
-            if(event.getType() == RenderGameOverlayEvent.ElementType.HOTBAR && player != null) {
+            @Nullable ClientPlayerEntity player = Minecraft.getInstance().player;
+
+            if(event.getType() == RenderGameOverlayEvent.ElementType.ALL && player != null) {
                 ItemStack playerHand;
                 if(player.getHeldItemMainhand().getItem() instanceof ShootableItem) {
                     playerHand = player.getHeldItemMainhand();
@@ -114,31 +81,61 @@ public class MainHUDQuiver {
                     return;
                 }
 
+                event.getMatrixStack().push();
+                event.getMatrixStack().translate(16, 16, 0);
+                Minecraft.getInstance().getTextureManager().bindTexture(WIDGETS);
+                AbstractGui.blit(event.getMatrixStack(), -12, -12, 0, 0, 24, 24, 36, 24);
+                event.getMatrixStack().pop();
+
                 List<ItemStack> readyArrows = findAmmos(player, playerHand);
+                List<Integer> skips = Lists.newLinkedList();
+                int xMultiplier = 0;
 
                 for(int i = 0; i < readyArrows.size(); ++ i) {
+                    if(skips.contains(i)) {
+                        continue;
+                    }
+
                     ItemStack readyArrow = readyArrows.get(i);
+                    int x = 24 * xMultiplier + 16, y = 16;
 
                     event.getMatrixStack().push();
-                    int x = 16 * (i + 1), y = 16;
-                    event.getMatrixStack().translate(x, y, 0);
+                    event.getMatrixStack().translate(x, y, 1);
                     event.getMatrixStack().scale(16, -16, 1);
                     IRenderTypeBuffer.Impl buffer = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
                     RenderSystem.enableBlend();
-
                     Minecraft.getInstance().getItemRenderer().renderItem(readyArrow, ItemCameraTransforms.TransformType.GUI, 15728880, OverlayTexture.NO_OVERLAY, event.getMatrixStack(), buffer);
                     RenderSystem.disableBlend();
                     buffer.finish();
                     event.getMatrixStack().pop();
-                    if(readyArrow.getCount() > 0) {
-                        event.getMatrixStack().push();
-                        if(player.isCreative() || readyArrow.getItem() instanceof ArrowItem && ((ArrowItem) readyArrow.getItem()).isInfinite(readyArrow, playerHand, player)) {
-                            AbstractGui.drawString(event.getMatrixStack(), Minecraft.getInstance().fontRenderer, new StringTextComponent("Infinity"), x, y + 16, 16777215);
+
+                    int count = readyArrow.getCount();
+
+                    for(int j = i + 1; j < readyArrows.size(); ++ j) {
+                        ItemStack nextArrow = readyArrows.get(j);
+                        if(nextArrow.isItemEqual(readyArrow) && ItemStack.areItemStackTagsEqual(nextArrow, readyArrow)) {
+                            count += nextArrow.getCount();
+                            skips.add(j);
                         } else {
-                            AbstractGui.drawString(event.getMatrixStack(), Minecraft.getInstance().fontRenderer, new StringTextComponent(String.valueOf(readyArrow.getCount())), x, y + 16, player.getItemInUseCount() > 0 ? 16777045 : 16777215);
+                            break;
                         }
-                        event.getMatrixStack().pop();
                     }
+
+                    event.getMatrixStack().push();
+                    if(player.isCreative() || readyArrow.getItem() instanceof ArrowItem && ((ArrowItem) readyArrow.getItem()).isInfinite(readyArrow, playerHand, player)) {
+                        event.getMatrixStack().push();
+                        event.getMatrixStack().translate(x + 3, y + 5, 0);
+                        Minecraft.getInstance().getTextureManager().bindTexture(WIDGETS);
+                        AbstractGui.blit(event.getMatrixStack(), -6, -4, 24, 0, 12, 8, 36, 24);
+                        event.getMatrixStack().pop();
+                    } else {
+                        boolean using = player.getItemInUseCount() > 0 && readyArrow == player.findAmmo(playerHand);
+                        String displayCount = using ? String.valueOf(count - 1) : String.valueOf(count); // TODO make text red is 0 is remaining
+                        AbstractGui.drawString(event.getMatrixStack(), Minecraft.getInstance().fontRenderer, new StringTextComponent(displayCount), x - 3, y + 1, using ? 16777045 : 16777215); // TODO anchor text to right alignment
+                    }
+                    event.getMatrixStack().pop();
+
+                    ++ xMultiplier;
                 }
             }
         }
