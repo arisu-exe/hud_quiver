8d4b6981a7f930d1dca6853add50087608e54bef
fallOut015 <captainDoodles264@gmail.com>
2021-03-04 23:23:17 -0800

    stuff

--- src/main/java/io/github/fallOut015/hud_quiver/MainHUDQuiver.java
+++ src/main/java/io/github/fallOut015/hud_quiver/MainHUDQuiver.java
@@ -1,12 +1,16 @@
 package io.github.fallOut015.hud_quiver;
 
 import com.google.common.collect.Lists;
+import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.fallOut015.hud_quiver.common.ConfigHUDQuiver;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.ItemStack;
@@ -14,13 +18,16 @@ import net.minecraft.item.Items;
 import net.minecraft.item.ShootableItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -34,6 +41,14 @@ public class MainHUDQuiver {
     private static @Nullable List<ItemStack> lastReadyArrows;
     private static final ResourceLocation WIDGETS;
 
+    // TODO everything configurable
+    // TODO slot switching with ctrl scroll and ctrl num keys (ctrl is configurable)
+    // TODO effects on arrow for flame, punch, and power
+    // TODO effects on arrow for piercing
+    // TODO render multiple arrows for each stack being combined
+    // TODO ugh i have to do localization now
+    // TODO config GUI
+
     static {
         interpolation = 0;
         lastHeld = null;
@@ -43,6 +58,7 @@ public class MainHUDQuiver {
 
     public MainHUDQuiver() {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigHUDQuiver.CLIENT_SPEC);
     }
 
     public static List<ItemStack> findAmmos(PlayerEntity player, ItemStack shootable) {
@@ -80,7 +96,6 @@ public class MainHUDQuiver {
     public static class Events {
         @OnlyIn(Dist.CLIENT)
         @SubscribeEvent
-        @SuppressWarnings("unused")
         public static void onRenderGameOverlay(final RenderGameOverlayEvent event) {
             @Nullable ClientPlayerEntity player = Minecraft.getInstance().player;
 
@@ -90,36 +105,51 @@ public class MainHUDQuiver {
                     playerHand = player.getHeldItemMainhand();
                     lastHeld = playerHand;
 
-                    if(interpolation < 1.0f) {
+                    if(interpolation < 1.0f && !Minecraft.getInstance().isGamePaused() && ConfigHUDQuiver.hides()) {
                         interpolation += 0.01f;
                     }
                 } else if(player.getHeldItemOffhand().getItem() instanceof ShootableItem) {
                     playerHand = player.getHeldItemOffhand();
                     lastHeld = playerHand;
 
-                    if(interpolation < 1.0f) {
+                    if(interpolation < 1.0f && !Minecraft.getInstance().isGamePaused() && ConfigHUDQuiver.hides()) {
                         interpolation += 0.01f;
                     }
                 } else {
-                    if(interpolation > 0) {
+                    if(!ConfigHUDQuiver.animates() && ConfigHUDQuiver.hides()) {
+                        return;
+                    }
+                    
+                    if(interpolation > 0 && !Minecraft.getInstance().isGamePaused() && ConfigHUDQuiver.hides()) {
                         interpolation -= 0.01f;
                     }
 
-                    if(lastHeld == null) {
-                        return;
+                    if(ConfigHUDQuiver.hides()) {
+                        if(lastHeld == null) {
+                            return;
+                        } else {
+                            playerHand = lastHeld;
+                        }
                     } else {
-                        playerHand = lastHeld;
+                        playerHand = lastHeld = new ItemStack(Items.BOW);
                     }
                 }
 
+                if(!ConfigHUDQuiver.hides()) {
+                    interpolation = 1.0f;
+                }
+
+                float left = ConfigHUDQuiver.getHorizontalOffset();
+                float top = ConfigHUDQuiver.getVerticalOffset();
+
                 event.getMatrixStack().push();
-                event.getMatrixStack().translate(16, bezier(interpolation, -16f, 16f), 0);
+                event.getMatrixStack().translate(left, ConfigHUDQuiver.animates() ? bezier(interpolation, -top, top) : top, 0);
                 Minecraft.getInstance().getTextureManager().bindTexture(WIDGETS);
                 AbstractGui.blit(event.getMatrixStack(), -12, -12, 0, 0, 24, 24, 36, 24);
                 event.getMatrixStack().pop();
 
                 List<ItemStack> readyArrows;
-                if(player.getHeldItemMainhand().getItem() instanceof ShootableItem || player.getHeldItemOffhand().getItem() instanceof ShootableItem) {
+                if(player.getHeldItemMainhand().getItem() instanceof ShootableItem || player.getHeldItemOffhand().getItem() instanceof ShootableItem || !ConfigHUDQuiver.hides()) {
                     readyArrows = findAmmos(player, playerHand);
                     lastReadyArrows = readyArrows;
                 } else {
@@ -127,8 +157,6 @@ public class MainHUDQuiver {
                 }
                 List<Integer> skips = Lists.newLinkedList();
                 int xMultiplier = 0;
-
-                // TODO render multiple arrows for each stack being combined
 
                 if(readyArrows != null) {
                     if(readyArrows.size() == 0) {
@@ -143,17 +171,52 @@ public class MainHUDQuiver {
                             }
 
                             ItemStack readyArrow = readyArrows.get(i);
-                            float x = 24 * xMultiplier + 16, y = bezier(interpolation, -16f, 16f);
+                            float x = 24 * xMultiplier + left, y = ConfigHUDQuiver.animates() ? bezier(interpolation, -top, top) : top;
 
                             event.getMatrixStack().push();
                             event.getMatrixStack().translate(x, y, i + 1);
                             event.getMatrixStack().scale(16, -16, 1);
+                            event.getMatrixStack().rotate(Vector3f.YP.rotationDegrees(180));
+                            event.getMatrixStack().rotate(Vector3f.XP.rotationDegrees(360));
                             IRenderTypeBuffer.Impl buffer = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
-                            // TODO brighten
-                            // TODO half opacity other arrows
-                            Minecraft.getInstance().getItemRenderer().renderItem(readyArrow, ItemCameraTransforms.TransformType.GUI, 16777215, OverlayTexture.NO_OVERLAY, event.getMatrixStack(), buffer);
+                            RenderSystem.enableDepthTest();
+                            RenderSystem.disableCull();
+                            Minecraft.getInstance().getItemRenderer().renderItem(readyArrow, ItemCameraTransforms.TransformType.FIXED, i == 0 ? 15728880 : 14540253, OverlayTexture.NO_OVERLAY, event.getMatrixStack(), buffer);
                             buffer.finish();
                             event.getMatrixStack().pop();
+                            RenderSystem.enableCull();
+                            RenderSystem.disableDepthTest();
+                            if(EnchantmentHelper.getEnchantmentLevel(Enchantments.MULTISHOT, playerHand) > 0) {
+                                event.getMatrixStack().push();
+                                event.getMatrixStack().translate(x - 4, y - 1, i + 1);
+                                event.getMatrixStack().scale(10, -10, 1);
+                                event.getMatrixStack().rotate(Vector3f.YP.rotationDegrees(180));
+                                event.getMatrixStack().rotate(Vector3f.XP.rotationDegrees(360));
+                                event.getMatrixStack().rotate(Vector3f.ZP.rotationDegrees(-30));
+                                IRenderTypeBuffer.Impl buffer2 = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+                                RenderSystem.enableDepthTest();
+                                RenderSystem.disableCull();
+                                Minecraft.getInstance().getItemRenderer().renderItem(readyArrow, ItemCameraTransforms.TransformType.FIXED, i == 0 ? 15728880 : 14540253, OverlayTexture.NO_OVERLAY, event.getMatrixStack(), buffer);
+                                buffer2.finish();
+                                event.getMatrixStack().pop();
+                                RenderSystem.enableCull();
+                                RenderSystem.disableDepthTest();
+
+                                event.getMatrixStack().push();
+                                event.getMatrixStack().translate(x + 1, y + 4, i + 1);
+                                event.getMatrixStack().scale(10, -10, 1);
+                                event.getMatrixStack().rotate(Vector3f.YP.rotationDegrees(180));
+                                event.getMatrixStack().rotate(Vector3f.XP.rotationDegrees(360));
+                                event.getMatrixStack().rotate(Vector3f.ZP.rotationDegrees(30));
+                                IRenderTypeBuffer.Impl buffer3 = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+                                RenderSystem.enableDepthTest();
+                                RenderSystem.disableCull();
+                                Minecraft.getInstance().getItemRenderer().renderItem(readyArrow, ItemCameraTransforms.TransformType.FIXED, i == 0 ? 15728880 : 14540253, OverlayTexture.NO_OVERLAY, event.getMatrixStack(), buffer);
+                                buffer3.finish();
+                                event.getMatrixStack().pop();
+                                RenderSystem.enableCull();
+                                RenderSystem.disableDepthTest();
+                            }
 
                             int count = readyArrow.getCount();
 
@@ -171,12 +234,12 @@ public class MainHUDQuiver {
                             if(player.isCreative() || readyArrow.getItem() instanceof ArrowItem && ((ArrowItem) readyArrow.getItem()).isInfinite(readyArrow, playerHand, player)) {
                                 event.getMatrixStack().translate(x + 3, y + 5, i + 1 + readyArrows.size());
                                 Minecraft.getInstance().getTextureManager().bindTexture(WIDGETS);
-                                AbstractGui.blit(event.getMatrixStack(), -6, -4, 24, 0, 12, 8, 36, 24);
+                                AbstractGui.blit(event.getMatrixStack(), -6, -4, 24, i == 0 ? 0 : 8, 12, 8, 36, 24);
                             } else {
                                 boolean using = player.getItemInUseCount() > 0 && readyArrow == player.findAmmo(playerHand) && player.getActiveItemStack().getItem() instanceof ShootableItem;
                                 String displayCount = using ? String.valueOf(count - 1) : String.valueOf(count);
                                 int length = displayCount.length();
-                                int color = using ? (count - 1 == 0 ? 16733525 : 16777045) : 16777215;
+                                int color = i == 0 ? (using ? (count - 1 == 0 ? 16733525 /*red*/ : 16777045 /*yellow*/) : 16777215 /*white*/) : 10066329 /*gray*/;
                                 event.getMatrixStack().translate(0, 0, i + 1 + readyArrows.size());
                                 AbstractGui.drawString(event.getMatrixStack(), Minecraft.getInstance().fontRenderer, new StringTextComponent(displayCount), Math.round(x + 9 - (6 * length)), Math.round(y + 1), color);
                             }
@@ -186,6 +249,15 @@ public class MainHUDQuiver {
                         }
                     }
                 }
+            }
+        }
+    }
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MODID)
+    public static class ModEvents {
+        @SubscribeEvent
+        public static void onModConfigEvent(final ModConfig.ModConfigEvent event) {
+            if(event.getConfig().getSpec() == ConfigHUDQuiver.CLIENT_SPEC) {
+                ConfigHUDQuiver.bakeConfig();
             }
         }
     }
---
+++ src/main/java/io/github/fallOut015/hud_quiver/common/ConfigHUDQuiver.java
@@ -0,0 +1,58 @@
+package io.github.fallOut015.hud_quiver.common;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class ConfigHUDQuiver {
+    public static final ClientConfig CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+
+    static {
+        final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
+
+    private static boolean animate = true;
+    private static boolean hide = true;
+    private static int horizontal_offset = 16;
+    private static int vertical_offset = 16;
+    // enum for docking side
+    // size
+
+    public static boolean animates() {
+        return animate;
+    }
+    public static boolean hides() {
+        return hide;
+    }
+    public static int getHorizontalOffset() {
+        return horizontal_offset;
+    }
+    public static int getVerticalOffset() {
+        return vertical_offset;
+    }
+
+    public static void bakeConfig() {
+        animate = CLIENT.ANIMATE.get();
+        hide = CLIENT.HIDE.get();
+        horizontal_offset = CLIENT.HORIZONTAL_OFFSET.get();
+        vertical_offset = CLIENT.VERTICAL_OFFSET.get();
+    }
+
+    public static class ClientConfig {
+        public final ForgeConfigSpec.BooleanValue ANIMATE;
+        public final ForgeConfigSpec.BooleanValue HIDE;
+        public final ForgeConfigSpec.IntValue HORIZONTAL_OFFSET;
+        public final ForgeConfigSpec.IntValue VERTICAL_OFFSET;
+
+        public ClientConfig(ForgeConfigSpec.Builder builder) {
+            //builder.push("hud_quiver");
+            this.ANIMATE = builder.comment("Animate the HUD Quiver showing and hiding.").translation("hud_quiver.config.animate").define("ANIMATE", true);
+            this.HIDE = builder.comment("Hide the HUD Quiver when not selecting a shootable item.").translation("hud_quiver.config.hide").define("HIDE", true);
+            this.HORIZONTAL_OFFSET = builder.comment("The margin on the left of the HUD Quiver.").translation("hud_quiver.config.horizontal_offset").defineInRange("HORIZONTAL_OFFSET", 16, 0, 255);
+            this.VERTICAL_OFFSET = builder.comment("The margin on the top of the HUD Quiver.").translation("hud_quiver.config.vertical_offset").defineInRange("VERTICAL_OFFSET", 16, 0, 255);
+            //builder.pop();
+        }
+    }
+}
